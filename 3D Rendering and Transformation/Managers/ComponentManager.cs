aee37b82658e89739936b33b379ce32046aebcb4
John Bosco Matanda <jbmatanda123@hotmail.com>
2018-03-28 23:17:22 +0200

    Implemeterat ComponentManager samt CameraComponent

---
+++ 3D Rendering and Transformation/Managers/ComponentManager.cs
@@ -0,0 +1,69 @@
+﻿using _3D_Rendering_and_Transformation.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3D_Rendering_and_Transformation.Managers
+{
+    public class ComponentManager
+    {
+        private static ComponentManager cm;
+        private uint nextid;
+        private Dictionary<Type, Dictionary<uint, IComponent>> Components = new Dictionary<Type, Dictionary<uint, IComponent>>();
+
+        private ComponentManager(){}
+
+        public static ComponentManager Get()
+        {
+            if(cm == null)
+            {
+                cm = new ComponentManager();
+            }
+            return cm;
+        }
+
+
+        public T EntityComponent<T>(uint id)
+        {
+            if (!Components.ContainsKey(typeof(T)))
+            {
+                Components.Add(typeof(T), new Dictionary<uint, IComponent>());
+            }
+            var components = Components[typeof(T)];
+            if (components.ContainsKey(id))
+            {
+                return (T)components[id];
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+        public Dictionary<uint, IComponent> GetComponents<TComponentType>()
+        {
+            if (!Components.ContainsKey(typeof(TComponentType)))
+            {
+                Components.Add(typeof(TComponentType), new Dictionary<uint, IComponent>());
+            }
+            return Components[typeof(TComponentType)];
+        }
+
+        public void RemoveComponents()
+        {
+            Components.Clear();
+        }
+
+        public void AddComponentsToEntity(IComponent component, uint id)
+        {
+            if (!Components.ContainsKey(component.GetType()))
+            {
+                Components.Add(component.GetType(), new Dictionary<uint, IComponent>());
+            }
+            var components = Components[component.GetType()];
+            components.Add(id, component);
+        }
+
+    }
+}
---
+++ 3D Rendering and Transformation/Systems/CameraSystem.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3D_Rendering_and_Transformation.Systems
+{
+    public class CameraSystem
+    {
+        public Vector3 camTarget { get; set; }
+        public Vector3 camPosition { get; set; }
+        public Matrix projectionMatrix { get; set; }
+        public Matrix viewMatrix { get; set; }
+        public Matrix worldMatrix { get; set; }
+    }
+}
